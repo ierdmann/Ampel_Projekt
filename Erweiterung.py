5decdcd3da0e97f8b06a948e6e87c9da51a66294
ierdmann <ilmarinenerdmann@gmail.com>
2018-11-18 14:14:37 +0100

    Update Erweiterung.py

--- Erweiterung.py
+++ Erweiterung.py
@@ -48,7 +48,7 @@ while schalter.value != True:
 	a_v_rot.turn_off()
 	a_f_rot.turn_off()
 	a_f_gruen.turn_off()
-	time.sleep(schalter.input)
+	input(schalter.value = True)
 
 	
 	if schalter.value == True:
