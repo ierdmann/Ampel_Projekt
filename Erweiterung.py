2df7d54c3aed62f407db41d26d319e68fd308782
ierdmann <ilmarinenerdmann@gmail.com>
2018-11-18 12:16:39 +0100

    Update Erweiterung.py

--- Erweiterung.py
+++ Erweiterung.py
@@ -74,7 +74,7 @@ if schalter.value == True:
 	a_v_gelb.turn_off()
 	print("Fussgaenger-phase beendet")
 
-elif schalter.value != True:
+elif schalter.value == False:
 	def countdown(n):
 		while n > 0:
 			print(time.sleep(1),"Zahl",n)
