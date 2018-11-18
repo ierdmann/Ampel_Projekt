a3bac1780954d26a8553f532295514a7a127fab4
ierdmann <ilmarinenerdmann@gmail.com>
2018-11-18 19:43:40 +0100

    Update Erweiterung.py

--- Erweiterung.py
+++ Erweiterung.py
@@ -33,22 +33,14 @@ atexit.register(ausschalten)
 
 # Hauptprogramm
 
-while schalter.value != True:
+while True:
 #	print("Initialisiere Durchfahrt Verkehr")
 	a_v_gruen.turn_on()
 	a_v_gelb.turn_off()
 	a_v_rot.turn_off()
 	a_f_rot.turn_on()
 	a_f_gruen.turn_off()
-	print("warten")
-	time.sleep(3)
-	print("schlafen")
-	a_v_gruen.turn_off()
-	a_v_gelb.turn_off()
-	a_v_rot.turn_off()
-	a_f_rot.turn_off()
-	a_f_gruen.turn_off()
-	break
+	
 
 	
 	if schalter.value == True:
@@ -75,7 +67,16 @@ while schalter.value != True:
 		a_v_rot.turn_off()
 		a_v_gelb.turn_off()
 		print("Fussgaenger-phase beendet")
-
+	else:
+		print("warten")
+		time.sleep(3)
+		print("schlafen")
+		a_v_gruen.turn_off()
+		a_v_gelb.turn_off()
+		a_v_rot.turn_off()
+		a_f_rot.turn_off()
+		a_f_gruen.turn_off()
+		break
 	
 
 		
