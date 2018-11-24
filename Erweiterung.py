30b02e428669137c53ae2f6e7689804e28d6e906
ierdmann <ilmarinenerdmann@gmail.com>
2018-11-24 22:22:29 +0100

    Update Erweiterung.py

--- Erweiterung.py
+++ Erweiterung.py
@@ -34,13 +34,6 @@ def ausschalten():
 atexit.register(ausschalten)
 
 def read():
-	while True:
-		a_v_gruen.turn_off()
-		a_v_gelb.turn_off()
-		a_v_rot.turn_off()
-		a_f_rot.turn_off()
-		a_f_gruen.turn_off()
-
 	if schalter.value == True:
 		print("Wieder_an")
 
