039370090d642a56af26864f3fbb9d1a92000058
ierdmann <ilmarinenerdmann@gmail.com>
2018-11-26 22:06:05 +0100

    Update Erweiterung.py

--- Erweiterung.py
+++ Erweiterung.py
@@ -45,7 +45,7 @@ def Zeit():
 		a_f_rot.turn_off()
 		a_f_gruen.turn_off()  						#Wenn die aktuelle Zeit minus der gespeicherten Zeit gleich 10. Dann Ampel aus.
 		print("Ampel_aus seit:", int(time.time())-n)
-		#eingabe
+		break
 	if schalter.value == True and int(time.time())-n>= 10:	
 		a_v_gruen.turn_off()
 		a_v_gelb.turn_off()
