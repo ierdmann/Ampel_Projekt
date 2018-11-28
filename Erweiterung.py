b079cb1dedb7a57c1143d6ba8e98d9fbcce0efbe
ierdmann <ilmarinenerdmann@gmail.com>
2018-11-28 21:37:01 +0100

    Update Erweiterung.py

--- Erweiterung.py
+++ Erweiterung.py
@@ -36,16 +36,25 @@ atexit.register(ausschalten)
 #eingabe = input(schalter)
 
 def Zeit():
-	# input(schalter)
-	
-	if int(time.time())-n >= 10 and schalter.value == False:
-		a_v_gruen.turn_off()
+	while True:
+		#print("Initialisiere Durchfahrt Verkehr")
+		a_v_gruen.turn_on()
+		#print("Ampel_Verkehr=gruen")
 		a_v_gelb.turn_off()
 		a_v_rot.turn_off()
-		a_f_rot.turn_off()
-		a_f_gruen.turn_off()  						#Wenn die aktuelle Zeit minus der gespeicherten Zeit gleich 10. Dann Ampel aus.
-		print("Ampel_aus seit:", int(time.time())-n, "Sekunden")
-		time.sleep(4)
+		a_f_rot.turn_on()
+		#print("Ampel_Fussgaenger=rot")
+		a_f_gruen.turn_off()
+		print(n)# input(schalter)
+	
+		if int(time.time())-n >= 10 and schalter.value == False:
+			a_v_gruen.turn_off()
+			a_v_gelb.turn_off()
+			a_v_rot.turn_off()
+			a_f_rot.turn_off()
+			a_f_gruen.turn_off()  						#Wenn die aktuelle Zeit minus der gespeicherten Zeit gleich 10. Dann Ampel aus.
+			print("Ampel_aus seit:", int(time.time())-n, "Sekunden")
+			time.sleep(4)
 		
 	
 # Hauptprogramm
@@ -87,16 +96,7 @@ while True:
 		a_v_gelb.turn_off()
 		print("Fussgaenger-phase beendet")
 		n = int(time.time())
-	else:
-		print("Initialisiere Durchfahrt Verkehr")
-		a_v_gruen.turn_on()
-		#print("Ampel_Verkehr=gruen")
-		a_v_gelb.turn_off()
-		a_v_rot.turn_off()
-		a_f_rot.turn_on()
-		#print("Ampel_Fussgaenger=rot")
-		a_f_gruen.turn_off()
-		print(n)	
+		
 
 	
 
